382f58c60629ecb61eeb575ba6b82d1b118c8f36
regimuci <regi.muci@gmail.com>
2020-04-20 15:27:43 +0200

    .

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,19 +1,3 @@
-<!--
-
-=========================================================
-* Now UI Dashboard - v1.5.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/now-ui-dashboard
-* Copyright 2019 Creative Tim (http://www.creative-tim.com)
-
-* Designed by www.invisionapp.com Coded by www.creative-tim.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
--->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,15 +23,12 @@
 <body class="">
 <div class="wrapper ">
     <div class="sidebar" data-color="blue">
-        <!--
-          Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
-      -->
         <div class="logo">
-            <a href="http://www.creative-tim.com" class="simple-text logo-mini">
-                CT
+            <a href="" class="simple-text logo-mini">
+
             </a>
-            <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-                Creative Tim
+            <a href="" class="simple-text logo-normal">
+                e-Bio Store
             </a>
         </div>
         <div class="sidebar-wrapper" id="sidebar-wrapper">
@@ -85,6 +66,7 @@
             </ul>
         </div>
     </div>
+
     <div class="main-panel" id="main-panel">
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
@@ -97,7 +79,7 @@
                             <span class="navbar-toggler-bar bar3"></span>
                         </button>
                     </div>
-                    <a class="navbar-brand" href="#pablo">Admin Panel</a>
+                    <a class="navbar-brand" href="">Admin Panel</a>
                 </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-bar navbar-kebab"></span>
@@ -105,29 +87,21 @@
                     <span class="navbar-toggler-bar navbar-kebab"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navigation">
-                    <form>
-                        <div class="input-group no-border">
-                            <input type="text" value="" class="form-control" placeholder="Search...">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <i class="now-ui-icons ui-1_zoom-bold"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+                    <!-- Search Form -->
+{{--                    <form>--}}
+{{--                        <div class="input-group no-border">--}}
+{{--                            <input type="text" value="" class="form-control" placeholder="Search...">--}}
+{{--                            <div class="input-group-append">--}}
+{{--                                <div class="input-group-text">--}}
+{{--                                    <i class="now-ui-icons ui-1_zoom-bold"></i>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
                     <ul class="navbar-nav">
-{{--                        <li class="nav-item">--}}
-{{--                            <a class="nav-link" href="#pablo">--}}
-{{--                                <i class="now-ui-icons media-2_sound-wave"></i>--}}
-{{--                                <p>--}}
-{{--                                    <span class="d-lg-none d-md-block">Stats</span>--}}
-{{--                                </p>--}}
-{{--                            </a>--}}
-{{--                        </li>--}}
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->first_name }}
-{{--                                <span class="caret"></span>--}}
                                 <i class="now-ui-icons users_single-02"></i>
                             </a>
 
@@ -143,27 +117,6 @@
                                 </form>
                             </div>
                         </li>
-{{--                        <li class="nav-item dropdown">--}}
-{{--                            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-{{--                                <i class="now-ui-icons location_world"></i>--}}
-{{--                                <p>--}}
-{{--                                    <span class="d-lg-none d-md-block">Some Actions</span>--}}
-{{--                                </p>--}}
-{{--                            </a>--}}
-{{--                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">--}}
-{{--                                <a class="dropdown-item" href="#">Action</a>--}}
-{{--                                <a class="dropdown-item" href="#">Another action</a>--}}
-{{--                                <a class="dropdown-item" href="#">Something else here</a>--}}
-{{--                            </div>--}}
-{{--                        </li>--}}
-{{--                        <li class="nav-item">--}}
-{{--                            <a class="nav-link" href="#pablo">--}}
-{{--                                <i class="now-ui-icons users_single-02"></i>--}}
-{{--                                <p>--}}
-{{--                                    <span class="d-lg-none d-md-block">Account</span>--}}
-{{--                                </p>--}}
-{{--                            </a>--}}
-{{--                        </li>--}}
                     </ul>
                 </div>
             </div>
@@ -174,36 +127,10 @@
         <div class="content">
             @yield('content')
         </div>
-        <footer class="footer">
-            <div class=" container-fluid ">
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="https://www.creative-tim.com">
-                                Creative Tim
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://presentation.creative-tim.com">
-                                About Us
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://blog.creative-tim.com">
-                                Blog
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="copyright" id="copyright">
-                    &copy; <script>
-                        document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-                    </script>, Designed by <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
-                </div>
-            </div>
-        </footer>
+
     </div>
 </div>
+
 <!--   Core JS Files   -->
 <script src="../assets/js/core/jquery.min.js"></script>
 <script src="../assets/js/core/popper.min.js"></script>
