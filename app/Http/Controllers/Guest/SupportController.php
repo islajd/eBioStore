1eb15c09d47c8cfa4afb30fdfca10c543e500fd3
Tea <tealico@hotmail.com>
2020-05-02 17:47:07 +0200

    Add Support

---
+++ app/Http/Controllers/Guest/SupportController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class SupportController extends Controller
+{
+    public function sendRequest(Request $request){
+        Mail::send('support.contact_email',
+            array(
+                'first_name' => $request->input('first_name'),
+                'last_name' => $request->input('last_name'),
+                'email' => $request->input('email'),
+                'phone_number' => $request->input('phone_number'),
+                'msg' => $request->input('message'),
+            ), function($message) use ($request)
+            {
+                $message->from('regi.muci@fshnstudent.info');
+                $message->subject('e-Bio Store Support');
+                $message->to('regi.muci@fshnstudent.info');
+            });
+
+        return back()->with('status', 'Thank you for contact us!');
+    }
+}
