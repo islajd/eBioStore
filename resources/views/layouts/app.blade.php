94e541c1df5b86549ed3c7c6abe8835ea3afd443
regimuci <regi.muci@gmail.com>
2020-05-03 20:43:12 +0200

    Fix Routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{route("Home")}}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'E-Bio Store') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -56,7 +56,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a class="nav-link" href="cart"><i class="fa fa-shopping-cart"></i> Cart</a>
+                                <a class="nav-link" href="{{route("Cart")}}"><i class="fa fa-shopping-cart"></i> Cart</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
