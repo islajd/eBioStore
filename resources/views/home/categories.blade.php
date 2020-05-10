b51e5edaf88ef2b45fdf06d07802f60c2f58f417
regimuci <regi.muci@gmail.com>
2020-05-10 18:05:43 +0200

    .

--- resources/views/home/categories.blade.php
+++ resources/views/home/categories.blade.php
@@ -7,9 +7,9 @@
         <div class="card-body">
             <ul class="list-group list-group-flush">
                 @foreach($categories as $category)
-                    <li class="list-group-item pl-0">
+                    <li class="list-group-item pl-0" style="color:{{ 'category/'.$category->id == request()->path() ? 'green': '' }}">
                         <i class="fa fa-angle-right" style="font-size: 15px;"></i>
-                        <a href="/category/{{$category->id}}" class="pl-1">{{$category->name}}</a>
+                        <a href="/category/{{$category->id}}" class="pl-1" style="color:{{ 'category/'.$category->id == request()->path() ? 'green': '' }}">{{$category->name}}</a>
                     </li>
                 @endforeach
             </ul>
